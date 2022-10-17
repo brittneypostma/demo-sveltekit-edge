b66be0f2ae06d113983609f7b709080bece8087e
brittneypostma <bdesignwebdev@gmail.com>
2022-10-17 18:00:24 -0400

    console.dir

--- src/routes/edge/+page.ts
+++ src/routes/edge/+page.ts
@@ -1,5 +1,6 @@
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
+	console.dir(event);
 	console.log(event.platform);
 	return {
 		res: event.platform
