87b92d886f6c8c5a9cc59a8064c4033d74890ce7
brittneypostma <bdesignwebdev@gmail.com>
2022-10-20 05:34:58 -0400

    ctx

--- src/routes/edge/+page.server.ts
+++ src/routes/edge/+page.server.ts
@@ -1,7 +1,34 @@
 import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async (event) => {
-	console.log(event.platform);
+	const ctx = event.platform.context;
+
 	return {
-		event: ''
+		event: ctx
 	};
+	// 5:32:48 AM: [render] {
+	//   context: {
+	//     cookies: {
+	//       delete: [Function: bound delete],
+	//       get: [Function: bound get],
+	//       set: [Function: bound set]
+	//     },
+	//     geo: {
+	//       city: "Holland",
+	//       country: { code: "US", name: "United States" },
+	//       subdivision: { code: "MI", name: "Michigan" }
+	//     },
+	//     ip: "2600:6c4a:747f:a7c4:507c:6160:872d:7f77",
+	//     json: [Function: bound json],
+	//     log: [Function],
+	//     next: [Function: next],
+	//     requestId: "01GFTBSXMXDWNSGJF0QTH0CRHZ",
+	//     rewrite: [Function: bound rewrite],
+	//     site: {
+	//       id: "846ed0e0-067a-474a-b074-56ad712b2272",
+	//       name: "demo-sveltekit-edge",
+	//       url: "http://demo-sveltekit-edge.netlify.app"
+	//     },
+	//     account: { id: "5d260577f54c73a4f797c5e1" }
+	//   }
+	// }
 };
